1f72547c1a02e660d88f4e912e706ea386326691
Paul Cowan <paul@custardsource.com>
2010-06-16 12:28:11 +1000

    Successfully failing build on any warnings

--- src/main/java/com/custardsource/MyMojo.java
+++ src/main/java/com/custardsource/MyMojo.java
@@ -1,81 +1,52 @@
 package com.custardsource;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
- * Goal which touches a timestamp file.
+ * Goal which retrieves a compiler warning count
  *
- * @goal touch
- * 
- * @phase process-sources
+ * @goal checkcount
+ * @phase compile
  */
 public class MyMojo
-    extends AbstractMojo
-{
+        extends AbstractMojo {
     /**
      * Location of the file.
-     * @parameter expression="${project.build.directory}"
+     *
+     * @parameter expression="${project.build.directory}/javac.out"
      * @required
      */
     private File outputDirectory;
 
+    private final Pattern pattern = Pattern.compile(".*" + Pattern.quote("warning: [") + ".*");
+
     public void execute()
-        throws MojoExecutionException
-    {
-        File f = outputDirectory;
-
-        if ( !f.exists() )
-        {
-            f.mkdirs();
+            throws MojoExecutionException {
+        if (!outputDirectory.exists()) {
+            throw new MojoExecutionException("Could not read file " + outputDirectory);
         }
 
-        File touch = new File( f, "touch.txt" );
-
-        FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
-
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
-                    w.close();
-                }
-                catch ( IOException e )
-                {
-                    // ignore
+        int warningCount = 0;
+        try {
+            for (String line : Files.readLines(outputDirectory, Charsets.UTF_8)) {
+                if (pattern.matcher(line).matches()) {
+                    warningCount++;
                 }
             }
+        } catch (IOException e) {
+            throw new MojoExecutionException("Could not parse file " + outputDirectory, e);
+        }
+
+        if (warningCount > 0) {
+            throw new MojoExecutionException(String.format("Failing build with warning count %s, no warnings permitted; see %s for warning details", warningCount, outputDirectory));
         }
     }
 }
