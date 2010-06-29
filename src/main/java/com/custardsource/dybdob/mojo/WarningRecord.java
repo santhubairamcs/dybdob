5055659d275b4823591f84ba328cddedb7d1c66a
Paul Cowan <paul@custardsource.com>
2010-06-29 17:37:10 +1000

    Actual implementation of warning tracking, now basically working.

---
+++ src/main/java/com/custardsource/dybdob/mojo/WarningRecord.java
@@ -0,0 +1,43 @@
+package com.custardsource.dybdob.mojo;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.apache.maven.project.MavenProject;
+
+@Entity
+public class WarningRecord {
+    @Id
+    private String id;
+
+    private String groupId;
+
+    private String artifactId;
+
+    private String version;
+
+    private Date dateLogged;
+
+    private int warningCount;
+
+    WarningRecord() {
+    }
+
+    static WarningRecord newRecord(MavenProject project, int warningCount) {
+        WarningRecord record = new WarningRecord();
+        record.groupId = project.getGroupId();
+        record.artifactId = project.getArtifactId();
+        record.version = project.getVersion();
+        record.dateLogged = new Date();
+        record.warningCount = warningCount;
+        record.id = UUID.randomUUID() + "-" + System.nanoTime();
+        return record;
+    }
+
+    public int warningCount() {
+        return warningCount;
+    }
+}
--- src/main/java/com/custardsource/dybdob/mojo/WarningTrackerMojo.java
+++ src/main/java/com/custardsource/dybdob/mojo/WarningTrackerMojo.java
@@ -1,9 +1,18 @@
 package com.custardsource.dybdob.mojo;
 
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-
-import java.io.File;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
 
 /**
  * Goal which fails the build if the warning count has increased since last successful execution
@@ -15,18 +24,112 @@ public class WarningTrackerMojo extends AbstractMojo {
     /**
      * Location of the file.
      *
-     * @parameter expression="${project.build.directory}/javac.out"
+     * @parameter expression="${projectWrapper.build.directory}/javac.out"
      * @required
      */
     private File warningLog;
 
+    /**
+     * DB driver to use when logging warnings
+     *
+     * @parameter expression="${dybdob.db.driver}"
+     * @required
+     */
+    private String jdbcDriver;
+
+    /**
+     * DB connection string to use when logging warnings
+     *
+     * @parameter expression="${dybdob.db.connection}"
+     * @required
+     */
+    private String jdbcConnection;
+
+    /**
+     * DB username to use when logging warnings
+     *
+     * @parameter expression="${dybdob.db.user}"
+     * @required
+     */
+    private String jdbcUser;
+
+    /**
+     * DB password to use when logging warnings
+     *
+     * @parameter expression="${dybdob.db.password}"
+     * @required
+     */
+    private String jdbcPassword;
+
+    /**
+     * Should we write our changes back to the database?
+     *
+     * @parameter expression="${dybdob.readonly}"
+     */
+    private boolean readOnly = true;
+
+    /**
+     * Hibernate dialect to use for writing changes
+     *
+     * @parameter expression="${dybdob.db.dialect}"
+     * @required
+     */
+    private String hibernateDialect;
+
+    /**
+     * @parameter default-value="${project}"
+     * */
+    private org.apache.maven.project.MavenProject mavenProject;
+
+    private HibernateTemplate hibernateTemplate;
+    private DriverManagerDataSource dataSource;
 
     public void execute() throws MojoExecutionException {
-        int oldCount = oldWarningCount();
+        setupDatabaseTemplate();
+        checkWarningCounts();
+    }
+
+    private void setupDatabaseTemplate() throws MojoExecutionException {
+        try {
+            Class.forName(jdbcDriver);
+        } catch (ClassNotFoundException e) {
+            throw new MojoExecutionException("Cannot load specified JDBC driver: " + jdbcDriver, e);
+        }
+        dataSource = new DriverManagerDataSource(jdbcConnection, jdbcUser, jdbcPassword);
+
+        AnnotationSessionFactoryBean sessionFactoryBean = new AnnotationSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        Properties config = new Properties();
+        config.setProperty("hibernate.dialect", hibernateDialect);
+        config.setProperty("hibernate.connection.autocommit", "true");
+        config.setProperty("hibernate.hbm2ddl.auto", "update");
+        sessionFactoryBean.setHibernateProperties(config);
+
+        sessionFactoryBean.setAnnotatedClasses(new Class<?>[]{WarningRecord.class});
+        try {
+            sessionFactoryBean.afterPropertiesSet();
+        } catch (Exception e) {
+            throw new MojoExecutionException("Could not set up database connection", e);
+        }
+        hibernateTemplate = new HibernateTemplate((SessionFactory) sessionFactoryBean.getObject());
+    }
+
+    private void checkWarningCounts() throws MojoExecutionException {
+        Integer oldCount = oldWarningCount();
         int warningCount = new WarningCounter(warningLog).warningCount();
-        if (warningCount < oldCount) {
+        if (oldCount == null) {
+            if (readOnly) {
+                getLog().warn(String.format("Unable to obtain old warning count; may be first run of this artifact version. New count would be %s", warningCount));
+            } else {
+                getLog().info(String.format("Unable to obtain old warning count; may be first run of this artifact version. New count is %s", warningCount));
+                lowerWarningCount(warningCount);
+            }
+        }
+        else if (warningCount < oldCount) {
             getLog().info(String.format("Well done! Warning count decreased from %s to %s", oldCount, warningCount));
-            lowerWarningCount();
+            if (!readOnly) {
+                lowerWarningCount(warningCount);
+            }
         } else if (oldCount == warningCount) {
             getLog().info(String.format("Warning count remains steady at %s", warningCount));
         } else {
@@ -34,10 +137,20 @@ public class WarningTrackerMojo extends AbstractMojo {
         }
     }
 
-    private void lowerWarningCount() {
+    private void lowerWarningCount(int warningCount) {
+        hibernateTemplate.save(WarningRecord.newRecord(mavenProject, warningCount));
     }
 
-    private int oldWarningCount() {
-        return 2;
+    private Integer oldWarningCount() {
+        DetachedCriteria c = DetachedCriteria.forClass(WarningRecord.class);
+        c.add(Restrictions.eq("groupId", mavenProject.getGroupId()));
+        c.add(Restrictions.eq("artifactId", mavenProject.getArtifactId()));
+        c.add(Restrictions.eq("version", mavenProject.getVersion()));
+        c.addOrder(Order.desc("dateLogged"));
+        List<WarningRecord> matches = hibernateTemplate.findByCriteria(c, 0, 1);
+        if (matches.isEmpty()) {
+            return null;
+        }
+        return matches.get(0).warningCount();
     }
 }
