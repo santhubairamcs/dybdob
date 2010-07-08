01a91062337bc6e426812a76c013a752e516b613
Paul Cowan <paul@custardsource.com>
2010-07-08 15:04:34 +1000

    Multiple detectors coming out of checkstyle

--- dybdob-core/src/main/java/com/custardsource/dybdob/CheckstyleDetector.java
+++ dybdob-core/src/main/java/com/custardsource/dybdob/CheckstyleDetector.java
@@ -5,9 +5,12 @@ import java.util.regex.Pattern;
 import com.google.common.collect.ImmutableMap;
 
 public class CheckstyleDetector extends LineMatchingDetector {
-    private static final Pattern ALL_ERRORS_PATTERN = Pattern.compile("<error.*");
+    private static final Pattern ALL_PATTERN = Pattern.compile("<error.*");
+    private static final Pattern ERROR_PATTERN = Pattern.compile("<error.*" + Pattern.quote("severity=\"error\"") + ".*");
+    private static final Pattern WARNING_PATTERN = Pattern.compile("<error.*" + Pattern.quote("severity=\"warning\"") + ".*");
+    private static final Pattern INFO_PATTERN = Pattern.compile("<error.*" + Pattern.quote("severity=\"info\"") + ".*");
 
     public CheckstyleDetector() {
-        super("checkstyle", ImmutableMap.of("all", ALL_ERRORS_PATTERN));
+        super("checkstyle", ImmutableMap.of("all", ALL_PATTERN, "error", ERROR_PATTERN, "warning", WARNING_PATTERN, "info", INFO_PATTERN));
     }
 }
